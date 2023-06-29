a646b2a2f7d6835071e75a09a9fef728d6ec1797
PedroAmarante0812 <86379291+PedroAmarante0812@users.noreply.github.com>
2023-06-28 23:54:50 -0300

    finalização com exe

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SpaceMaker
+
+Pedro Amarante - 1134859
+Gabriel Bolson - 1134814
+
+Link para download:
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 import cx_Freeze
-executables = [cx_Freeze.Executable(script="main.py",icon="space.ico")]
+
+executables = [cx_Freeze.Executable(script="main.py", icon="space.png")]
+
 cx_Freeze.setup(
-name="Space Marker",
-options={"build_exe": {"packages":["pygame"],
-"include_files":["bg.jpg, estrela.png, nave.png, Space_Machine_Power.mp3, Space_sound.mp3, space.ico, space.png"]}},
-executables = executables
+    name="Space Marker",
+    version = "1.0",
+    options={"build_exe":
+        {"packages":
+            ["pygame", "tkinter", "os"],"include_files":["bg.jpg", "estrela.png", "nave.png", "Space_Machine_Power.mp3", "Space_sound.mp3", "space.ico", "space.png"]}},
+    executables = executables
 )
